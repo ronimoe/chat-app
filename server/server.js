998770f87d4605cd710e86461dad1923d0bcbfcb
Roni <roni@Ronis-MacBook-Pro.local>
2018-06-09 02:26:43 +0700

    second commit

--- server/server.js
+++ server/server.js
@@ -11,4 +11,6 @@ app.get('/', (req, res) => {
   res.sendFile(publicPath);
 });
 
-app.listen(port, () => console.log(`Server running at port ${port}`));
+app.listen(port, () => {
+  console.log(`Server running at port: ${port}`);
+});
