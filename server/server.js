8d3b6f73a8f77c38f6a38eeff61009823f7bf498
Roni <roni@Ronis-MacBook-Pro.local>
2018-06-09 02:19:39 +0700

    first commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static(publicPath));
+
+app.get('/', (req, res) => {
+  res.sendFile(publicPath);
+});
+
+app.listen(port, () => console.log(`Server running at port ${port}`));
